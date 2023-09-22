b724e2ebefb5980a36a6538edd3f7b3b6049fcff
paramgit85 <paramasudan@gmail.com>
2023-09-22 15:48:01 +0530

    sum modified

--- ConsoleAppDemo2/Program.cs
+++ ConsoleAppDemo2/Program.cs
@@ -11,12 +11,15 @@ namespace ConsoleAppDemo2
         static void Main(string[] args)
         {
 
-            static void Sum()
+            
+
+
+            static void Sum(int first, int second)
             {
-                int first = 5;
-                int second = 10;
                 int sum = first + second;
             }
+
+
 
         }
     }
